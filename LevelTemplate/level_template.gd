6881d18122f2db9b648ce94a771ac826ceb1411d
Yeiner P <m78tkm@gmail.com>
2025-09-06 13:18:28 -0500

    logic of the game added and working (questions and answer working even answer screen)

--- LevelTemplate/level_template.gd
+++ LevelTemplate/level_template.gd
@@ -1,1 +1,81 @@
 extends Node2D
+
+
+@onready var LabelMainQuest: Label = $ControlObjs/LabelQuestions/LabelQuest
+@onready var ImgQ1: TextureRect = $ControlObjs/ImgAnswers/GridContainer/ImgOption1/VBoxContainer/Img
+@onready var ImgQ2: TextureRect = $ControlObjs/ImgAnswers/GridContainer/ImgOption2/VBoxContainer/Img
+@onready var ImgQ3: TextureRect = $ControlObjs/ImgAnswers/GridContainer/ImgOption3/VBoxContainer/Img
+@onready var ImgQ4: TextureRect = $ControlObjs/ImgAnswers/GridContainer/ImgOption4/VBoxContainer/Img
+
+@onready var respons_screen: Control = $CanvasLayer/ResponsScreen
+
+var DicQuestion1: Dictionary = {
+	"Question": "¿Cuál de estos códigos genera la secuencia 2 - 4 - 6 - 8 - 10?",
+	"Img1": load("res://Assets/2 - 4 - 6 - 8 - 10/Screenshot 2025-08-22 173045.png"),
+	"Img2": load("res://Assets/2 - 4 - 6 - 8 - 10/Screenshot 2025-08-22 173126.png"),
+	"Img3": load("res://Assets/2 - 4 - 6 - 8 - 10/Screenshot 2025-08-22 173145.png"),
+	"Img4": load("res://Assets/2 - 4 - 6 - 8 - 10/Screenshot 2025-08-22 173213.png"),
+	"QCorrect": "Op1"
+}
+
+var CorrectAsw: String
+var UserRespond: String
+var CanRespondOneTime: bool = true
+
+
+func _ready() -> void:
+	respons_screen.ResetPanel()
+	
+	LoadQuestions(DicQuestion1)#Take out later
+
+
+func LoadQuestions(dicQ: Dictionary) -> void:
+	LabelMainQuest.text = dicQ.Question
+	ImgQ1.texture = dicQ.Img1
+	ImgQ2.texture = dicQ.Img2
+	ImgQ3.texture = dicQ.Img3
+	ImgQ4.texture = dicQ.Img4
+	CorrectAsw = dicQ.QCorrect
+
+
+func _on_button_pressed_Op1() -> void:
+	CheckCorrectAnws("Op1")
+	print("op1")
+
+
+func _on_button_pressed_Op2() -> void:
+	CheckCorrectAnws("Op2")
+	print("Op2")
+
+
+func _on_button_pressed_Op3() -> void:
+	CheckCorrectAnws("Op3")
+	print("Op3")
+
+
+func _on_button_pressed_Op4() -> void:
+	CheckCorrectAnws("Op4")
+	print("Op4")
+
+
+func CheckCorrectAnws(respon: String) -> void:
+	if CanRespondOneTime:
+		CanRespondOneTime = false
+		UserRespond = respon
+		
+		if UserRespond == CorrectAsw:
+			ShowCorrectAnim()
+		else:
+			ShowIncorrectAnim()
+
+
+func ShowCorrectAnim() -> void:
+	respons_screen.ShowAnwer(1)
+
+
+func ShowIncorrectAnim() -> void:
+	respons_screen.ShowAnwer(0)
+
+
+func _on_respons_screen_accept() -> void:
+	CanRespondOneTime = true
--- ResponsScreen/respons_screen.gd
+++ ResponsScreen/respons_screen.gd
@@ -1,18 +1,22 @@
 extends Control
 
+signal accept
+
 @onready var BG_mesage: ColorRect = $ColorRect
 @onready var CorrectImg: TextureRect = $ColorRect/Correct
 @onready var IncorrectImg: TextureRect = $ColorRect/Incorrect
 @onready var button_accept: Button = $ButtonAccept
+@onready var Bg: ColorRect = $BG
 
 var Message: Array
 
 func _ready() -> void:
+	visible = false
 	BG_mesage.visible = false
+	Bg.visible = false
 	Message = [IncorrectImg, CorrectImg]
 	ResetPanel()
 	button_accept.visible = false
-	ShowAnwer(1)
 
 
 func ShowAnwer(v: int) -> void:
@@ -26,7 +30,9 @@ func ShowAnwer(v: int) -> void:
 func AnimShowMessage(v: int) -> void:
 	var tween: Tween = get_tree().create_tween()
 	
+	visible = true
 	var ImgAnwer: TextureRect = Message[v]
+	Bg.visible = true
 	BG_mesage.visible = true
 	ImgAnwer.visible = true
 	ImgAnwer.scale = Vector2(0.4, 0.4)
@@ -35,12 +41,14 @@ func AnimShowMessage(v: int) -> void:
 	
 	ShowButton()
 	
-	await tween.tween_interval(8).finished
-	ResetPanel()
+	#await tween.tween_interval(8).finished
+	#ResetPanel()
 
 
 func ResetPanel() -> void:
+	visible = false
 	BG_mesage.visible = false
+	Bg.visible = false
 	for i: TextureRect in Message:
 		i.visible = false
 		i.scale = Vector2(0.4, 0.4)
@@ -54,3 +62,4 @@ func ShowButton() -> void:
 func _on_button_accept_pressed() -> void:
 	ResetPanel()
 	button_accept.visible = false
+	emit_signal("accept")
